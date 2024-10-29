c2395505744490ebd873e204c7a673469b290b3a
Caio Tavares <102376017+Caio-T4vares@users.noreply.github.com>
2024-10-29 13:55:50 -0300

    chore: add ts-node-dev and path mapping

--- src/frameworks/express/index.ts
+++ src/frameworks/express/index.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { dataSource } from '../typeorm'
+import { dataSource } from '@frameworks/typeorm'
 
 const app = express()
 const PORT = process.env.PORT || 3000
