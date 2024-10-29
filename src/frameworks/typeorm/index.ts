efac841fb84adc60c2f06d0010c2b9f58242e988
Kaio <kaiofernando05cont@gmail.com>
2024-10-29 19:45:04 -0300

    feat: add migration for orders table

--- src/frameworks/typeorm/index.ts
+++ src/frameworks/typeorm/index.ts
@@ -1,14 +1,15 @@
 import { DataSource } from 'typeorm'
 import { CreateCars1730228276828 } from './migrations/1730228276828-CreateCars'
 import { CreateItems1730228906205 } from './migrations/1730228906205-CreateItems'
+import { CreateOrders1730237813958 } from './migrations/1730237813958-CreateOrders'
 
 export const dataSource = new DataSource({
   type: 'postgres',
   host: 'localhost',
-  username: 'username',
-  password: 'password',
+  username: 'postgres',
+  password: 'kaio0510',
   database: 'compasscar',
   port: 5432,
-  migrations: [CreateCars1730228276828, CreateItems1730228906205],
+  migrations: [CreateCars1730228276828, CreateItems1730228906205, CreateOrders1730237813958],
   entities: [],
 })
