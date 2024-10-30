b6384699686d435c7d3bb238940f820f0f7b67dd
Caio Tavares <102376017+Caio-T4vares@users.noreply.github.com>
2024-10-30 17:54:41 -0300

    feat: add delete car controller

---
+++ src/cars/infraestructure/http/controllers/delete-car.controller.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express'
+import { CarsTypeormRepository } from '../../typeorm/repositories/cars-typeorm.repository'
+import { dataSource } from '@/common/infraestructure/typeorm'
+import { Car } from '../../typeorm/entities/cars.entity'
+import { DeleteCarUseCase } from '@/cars/usecases/delete-car.usecase'
+
+export async function deleteCarController(req: Request, res: Response) {
+  const { id } = req.params
+  const deleteCarUseCase = new DeleteCarUseCase(
+    new CarsTypeormRepository(dataSource.getRepository(Car)),
+  )
+
+  const deletedCar = await deleteCarUseCase.execute(id)
+
+  return res.status(201).json(deletedCar)
+}
