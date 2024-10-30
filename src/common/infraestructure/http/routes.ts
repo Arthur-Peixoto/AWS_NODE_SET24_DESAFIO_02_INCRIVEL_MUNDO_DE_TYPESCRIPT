3eb78e5f3969eb2b03cd74425133c1ad0093c067
Caio Tavares <102376017+Caio-T4vares@users.noreply.github.com>
2024-10-30 14:46:49 -0300

    fix: add car routes to general router

--- src/common/infraestructure/http/routes.ts
+++ src/common/infraestructure/http/routes.ts
@@ -1,5 +1,8 @@
+import { carRoutes } from '@/cars/infraestructure/http/routes/cars.route'
 import { Router } from 'express'
 
 const routes = Router()
+
+routes.use('/cars', carRoutes)
 
 export default routes
