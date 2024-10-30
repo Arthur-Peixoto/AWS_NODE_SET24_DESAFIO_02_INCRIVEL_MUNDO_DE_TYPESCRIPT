aa67f666d2bb83e9d47167a7c480c888824f0c77
Caio Tavares <102376017+Caio-T4vares@users.noreply.github.com>
2024-10-30 14:47:18 -0300

    feat: add entities to datasource

--- src/common/infraestructure/typeorm/index.ts
+++ src/common/infraestructure/typeorm/index.ts
@@ -1,6 +1,8 @@
 import { DataSource } from 'typeorm'
 import { CreateCars1730228276828 } from './migrations/1730228276828-CreateCars'
 import { CreateItems1730228906205 } from './migrations/1730228906205-CreateItems'
+import { Car } from '@/cars/infraestructure/typeorm/entities/cars.entity'
+import { Item } from '@/cars/infraestructure/typeorm/entities/items.entity'
 
 export const dataSource = new DataSource({
   type: 'postgres',
@@ -10,5 +12,5 @@ export const dataSource = new DataSource({
   database: 'compasscar',
   port: 5432,
   migrations: [CreateCars1730228276828, CreateItems1730228906205],
-  entities: [],
+  entities: [Car, Item],
 })
