87188359a374df72835e28add317514be13143f7
Caio Tavares <102376017+Caio-T4vares@users.noreply.github.com>
2024-10-30 15:40:47 -0300

    feat: add route for read cars

--- src/cars/infraestructure/http/routes/cars.route.ts
+++ src/cars/infraestructure/http/routes/cars.route.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express'
 import { createCarController } from '../controllers/create-car.controller'
+import { readCarController } from '../controllers/read-car.controller'
 
 const carRoutes = Router()
 
 carRoutes.post('/', (req, res) => {
   createCarController(req, res)
+})
+
+carRoutes.get('/', (req, res) => {
+  readCarController(req, res)
 })
 
 export { carRoutes }
