7cb8cd5ab6ca96bfb8aacafab2e9d51fbf6a1cda
Caio Tavares <102376017+Caio-T4vares@users.noreply.github.com>
2024-10-30 17:48:42 -0300

    fix: fix returned json in update use case

--- src/cars/infraestructure/http/routes/cars.route.ts
+++ src/cars/infraestructure/http/routes/cars.route.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { createCarController } from '../controllers/create-car.controller'
 import { readCarController } from '../controllers/read-car.controller'
+import { updateCarController } from '../controllers/update-car.controller'
 
 const carRoutes = Router()
 
@@ -10,6 +11,10 @@ carRoutes.post('/', (req, res) => {
 
 carRoutes.get('/', (req, res) => {
   readCarController(req, res)
+})
+
+carRoutes.patch('/:id', (req, res) => {
+  updateCarController(req, res)
 })
 
 export { carRoutes }
--- src/cars/usecases/update-car.usecase.ts
+++ src/cars/usecases/update-car.usecase.ts
@@ -28,23 +28,46 @@ export type UpdateCarOutput = {
 export class UpdateCarUseCase {
   constructor(private carRepository: CarsRepository) {}
 
-  async execute(id: string, input: UpdateCarInput): Promise<UpdateCarOutput> {
+  async execute(
+    id: string,
+    {
+      model,
+      brand,
+      year,
+      licensePlate,
+      items,
+      status,
+      mileage,
+      price,
+    }: UpdateCarInput,
+  ): Promise<UpdateCarOutput> {
     const carExists = await this.carRepository.findById(id)
 
     if (!carExists) {
       throw new Error("Car don't exist")
     }
 
-    if (input.licensePlate) {
-      const duplicatedCar = this.carRepository.findByLicensePlate(
-        input.licensePlate,
-      )
-      if (duplicatedCar) {
+    if (licensePlate) {
+      const duplicatedCar =
+        await this.carRepository.findByLicensePlate(licensePlate)
+      if (duplicatedCar && duplicatedCar.id !== carExists.id) {
         throw new Error('Invalid license plate!')
       }
     }
 
-    const car = { ...carExists, ...input }
+    const car = {
+      id: carExists.id,
+      registrationDate: carExists.registrationDate,
+      model: model === undefined ? carExists.model : model,
+      brand: brand === undefined ? carExists.brand : brand,
+      mileage: mileage === undefined ? carExists.mileage : mileage,
+      year: year === undefined ? carExists.year : year,
+      price: price === undefined ? carExists.price : price,
+      status: status === undefined ? carExists.status : status,
+      items: items === undefined ? carExists.items : items,
+      licensePlate:
+        licensePlate === undefined ? carExists.licensePlate : licensePlate,
+    }
 
     const updatedCar = this.carRepository.update(car)
 
