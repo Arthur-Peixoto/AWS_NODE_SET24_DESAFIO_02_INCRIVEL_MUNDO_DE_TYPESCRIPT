1d8b10d2ac38fe49a3573614567c379e7980b380
Caio Tavares <102376017+Caio-T4vares@users.noreply.github.com>
2024-10-30 14:45:28 -0300

    feat: add car controller

---
+++ src/cars/infraestructure/http/controllers/create-car.controller.ts
@@ -0,0 +1,26 @@
+import { CreateCarUseCase } from '@/cars/usecases/create-car.usecase'
+import { Request, Response } from 'express'
+import { CarsTypeormRepository } from '../../typeorm/repositories/cars-typeorm.repository'
+import { dataSource } from '@/common/infraestructure/typeorm'
+import { Car } from '../../typeorm/entities/cars.entity'
+
+export async function createCarController(req: Request, res: Response) {
+  const { model, brand, year, mileage, status, licensePlate, items, price } =
+    req.body
+
+  const createCarUseCase = new CreateCarUseCase(
+    new CarsTypeormRepository(dataSource.getRepository(Car)),
+  )
+  const newCar = await createCarUseCase.execute({
+    model,
+    brand,
+    year,
+    mileage,
+    status,
+    licensePlate,
+    items,
+    price,
+  })
+
+  return res.status(201).json(newCar)
+}
