8bd4780d6e3b8268006ae2cd6acd0a30ad830c5d
Arthur-Peixoto <ggpeaol@gmail.com>
2024-10-31 12:08:58 -0300

    feat: add get user route

--- src/users/infraestructure/http/routes/user.routes.ts
+++ src/users/infraestructure/http/routes/user.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express'
 import { createUserController } from '@/users/infraestructure/http/controllers/create-user.controllers'
+import { listUsersController } from '../controllers/read-users.controller'
 const userRoutes = Router()
 
 userRoutes.post('/users/', (req, res) => {
   createUserController(req, res)
 })
+
+userRoutes.get('/users/', (req, res) => {
+  listUsersController(req, res);
+});
 
 export { userRoutes }
