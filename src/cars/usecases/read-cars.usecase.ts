2c44cc2f2d17ed35f52d74f8f6a368713786e54b
Caio Tavares <102376017+Caio-T4vares@users.noreply.github.com>
2024-10-31 16:01:06 -0300

    refactor: change name of classes and types of read-cars usecase

--- src/cars/usecases/read-cars.usecase.ts
+++ src/cars/usecases/read-cars.usecase.ts
@@ -2,7 +2,7 @@ import { CarModel } from '../domain/models/cars.model'
 import { ItemModel } from '../domain/models/items.model'
 import { CarsRepository } from '../domain/repositories/cars.repository'
 
-export type readCarInput = {
+export type readCarsInput = {
   page?: number
   per_page?: number
   model?: string
@@ -16,17 +16,17 @@ export type readCarInput = {
   items?: ItemModel[]
 }
 
-export type readCarOutput = {
+export type readCarsOutput = {
   per_page: number
   page: number
   count: number
   data: CarModel[]
 }
 
-export class ReadCarUseCase {
+export class ReadCarsUseCase {
   constructor(private carRepository: CarsRepository) {}
 
-  async execute(input: readCarInput): Promise<readCarOutput> {
+  async execute(input: readCarsInput): Promise<readCarsOutput> {
     // verificar placa e status --> não permite carro com placa igual e status = ativo
     const searchResults = await this.carRepository.findAllAndFilter(input)
 
