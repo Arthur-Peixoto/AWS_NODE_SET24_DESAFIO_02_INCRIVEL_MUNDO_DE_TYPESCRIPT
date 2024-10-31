b7db7bb3b4402c4b63ac7d627dde983ff6099e05
Arthur-Peixoto <ggpeaol@gmail.com>
2024-10-31 11:56:06 -0300

    feat: add filters for list users

--- src/users/domain/repositories/users.repository.ts
+++ src/users/domain/repositories/users.repository.ts
@@ -1,17 +1,32 @@
-import { RepositoryInterface } from '@/common/domain/repositories/repository.interface'
-import { UserModel } from "@/users/domain/models/users.model"
+// src/users/domain/repositories/user.repository.ts
+
+import { RepositoryInterface } from '@/common/domain/repositories/repository.interface';
+import { UserModel } from '@/users/domain/models/users.model';
 
 export type CreateUserProps = {
-  id?: string
-  fullName: string
-  email: string
-  password: string
-  registrationDate?: Date
-  deletionDate?: Date | null
-}
+  id?: string;
+  fullName: string;
+  email: string;
+  password: string;
+  registrationDate?: Date;
+  deletionDate?: Date | null;
+};
+
+export type UserFilterParams = {
+  name?: string;
+  email?: string;
+  deleted?: boolean;
+};
+
+
 
 export interface UserRepository
   extends RepositoryInterface<UserModel, CreateUserProps> {
-  findByID(id: string): Promise<UserModel | null>
-  findByEmail(email: string): Promise<UserModel | null>
+  findByID(id: string): Promise<UserModel | null>;
+  findByEmail(email: string): Promise<UserModel | null>;
+  
+  findAllWithFilters(
+    filters?: UserFilterParams,
+    
+  ): Promise<{ users: UserModel[]; total: number }>;
 }
