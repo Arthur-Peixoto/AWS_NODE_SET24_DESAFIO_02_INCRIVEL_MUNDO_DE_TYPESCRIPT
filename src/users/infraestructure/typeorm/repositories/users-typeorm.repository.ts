a8114e8100b82df470b1e613f49762c59e225452
Arthur-Peixoto <ggpeaol@gmail.com>
2024-10-31 12:00:17 -0300

    feat: add list with params in user typeorm repository

--- src/users/infraestructure/typeorm/repositories/users-typeorm.repository.ts
+++ src/users/infraestructure/typeorm/repositories/users-typeorm.repository.ts
@@ -24,5 +24,35 @@ export class UsersTypeormRepository implements UserRepository {
     return await this.userRepository.findOne({ where: { email } });
   }
 
-  
+  async findAllWithFilters(
+    filters: UserFilterParams = {},
+    orderBy: UserOrderByParams[] = [],
+    pagination: UserPaginationParams = { page: 1, perPage: 10 }
+  ): Promise<{ users: User[]; total: number }> {
+    const queryBuilder = this.userRepository.createQueryBuilder('user');
+
+    if (filters.name) {
+      queryBuilder.andWhere('user.fullName ILIKE :name', { name: `%${filters.name}%` });
+    }
+    if (filters.email) {
+      queryBuilder.andWhere('user.email ILIKE :email', { email: `%${filters.email}%` });
+    }
+    if (filters.deleted !== undefined) {
+      if (filters.deleted) {
+        queryBuilder.andWhere('user.deletionDate IS NOT NULL');
+      } else {
+        queryBuilder.andWhere('user.deletionDate IS NULL');
+      }
+    }
+
+    for (const order of orderBy) {
+      queryBuilder.addOrderBy(`user.${order.field}`, order.direction);
+    }
+
+    const total = await queryBuilder.getCount();
+    queryBuilder.skip((pagination.page - 1) * pagination.perPage).take(pagination.perPage);
+
+    const users = await queryBuilder.getMany();
+    return { users, total };
+  }
 }
