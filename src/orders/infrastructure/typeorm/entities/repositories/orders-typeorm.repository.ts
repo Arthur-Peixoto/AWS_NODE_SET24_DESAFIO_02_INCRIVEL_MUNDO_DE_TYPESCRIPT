7602cc8776d91c159e1cfda9a279a4d0fe34858b
Kaio <kaiofernando05cont@gmail.com>
2024-11-01 00:37:28 -0300

    feat: implement orders repository in typeorm

---
+++ src/orders/infrastructure/typeorm/entities/repositories/orders-typeorm.repository.ts
@@ -0,0 +1,61 @@
+// TODO: Configurar retorno de erros da aplicação
+
+import { OrderModel } from '@/orders/domain/models/orders.model'
+import {
+  OrdersRepository,
+  CreateOrderProps,
+  findParams,
+  findResults,
+} from '@/orders/domain/repositories/orders.repository'
+import { Repository } from 'typeorm'
+import { Order } from '../entities/orders.entity'
+
+export class OrdersTypeormRepository implements OrdersRepository {
+  constructor(private ordersRepository: Repository<Order>) {}
+  // TODO: findAllAndFilter
+  async findAllAndFilter(params: findParams): Promise<findResults> {
+    const id: string = params.id
+    const [data, count] = await this.ordersRepository.findAndCount({
+      where: { id },
+    })
+    return {
+      per_page: 0,
+      page: 0,
+      count: count,
+      data,
+    }
+  }
+
+  create(props: CreateOrderProps): OrderModel {
+    return this.ordersRepository.create(props)
+  }
+
+  async insert(model: OrderModel): Promise<OrderModel> {
+    return await this.ordersRepository.save(model)
+  }
+
+  async findById(id: string): Promise<OrderModel> {
+    const order = await this.ordersRepository.findOne({
+      where: { id: id },
+      // relations: { client: true },
+    })
+
+    // if (!order) {
+    //   throw new EntityNotFoundError(`Order not found using id ${id}`)
+    // }
+
+    return order
+  }
+
+  async update(model: OrderModel): Promise<OrderModel> {
+    await this.findById(model.id)
+    await this.ordersRepository.update({id: model.id}, model)
+    return model
+  }
+
+  async delete(model: OrderModel): Promise<OrderModel> {
+    await this.findById(model.id)
+    await this.ordersRepository.delete(model.id)
+    return model
+  }
+}
