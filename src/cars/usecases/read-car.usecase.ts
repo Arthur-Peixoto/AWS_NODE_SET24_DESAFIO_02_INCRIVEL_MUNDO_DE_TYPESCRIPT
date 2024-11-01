d6cc67f5cdc18d7b0575bc3de6298d0cf298b7c3
Caio Tavares <102376017+Caio-T4vares@users.noreply.github.com>
2024-11-01 14:55:25 -0300

    fix: change format of items response

--- src/cars/usecases/read-car.usecase.ts
+++ src/cars/usecases/read-car.usecase.ts
@@ -1,11 +1,24 @@
-import { CarModel } from '../domain/models/cars.model'
 import { CarsRepository } from '../domain/repositories/cars.repository'
+
+export type readCarOutput = {
+  id: string
+  model: string
+  brand: string
+  licensePlate: string
+  mileage?: number
+  year: number
+  items: string[]
+  price: number
+  registrationDate: Date
+  status: 'ativo' | 'inativo' | 'excluído'
+}
 
 export class ReadCarUseCase {
   constructor(private carRepository: CarsRepository) {}
 
-  async execute(id: string): Promise<CarModel> {
+  async execute(id: string): Promise<readCarOutput> {
     const car = await this.carRepository.findById(id)
-    return car
+    const itemsName = car.items.map((item) => item.name)
+    return { ...car, items: itemsName }
   }
 }
