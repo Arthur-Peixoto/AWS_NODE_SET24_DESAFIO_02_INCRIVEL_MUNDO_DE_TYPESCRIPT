154f32a59ebe2162f49c13cac4d020779c14e59a
Caio Tavares <102376017+Caio-T4vares@users.noreply.github.com>
2024-11-01 16:33:19 -0300

    feat: add validation for create car

--- src/cars/infraestructure/http/routes/cars.route.ts
+++ src/cars/infraestructure/http/routes/cars.route.ts
@@ -4,12 +4,36 @@ import { readCarController } from '../controllers/read-car.controller'
 import { updateCarController } from '../controllers/update-car.controller'
 import { deleteCarController } from '../controllers/delete-car.controller'
 import { readCarsController } from '../controllers/read-cars.controller'
+import { celebrate, Joi, Segments } from 'celebrate'
 
+const CURRENT_YEAR = new Date().getFullYear()
 const carRoutes = Router()
 
-carRoutes.post('/', (req, res) => {
-  createCarController(req, res)
-})
+carRoutes.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      model: Joi.string().required(),
+      brand: Joi.string().required(),
+      year: Joi.number()
+        .integer()
+        .min(CURRENT_YEAR - 11)
+        .max(CURRENT_YEAR)
+        .required(),
+      licensePlate: Joi.string()
+        .length(8)
+        .pattern(/^[A-Za-z]{3}-[0-9]{4}$/)
+        .required(),
+      mileage: Joi.number().positive().required(),
+      items: Joi.array().items(Joi.string()).max(5).unique().required(),
+      price: Joi.number().positive().precision(2).required(),
+      status: Joi.string().valid('ativo', 'inativo').required(),
+    },
+  }),
+  (req, res) => {
+    createCarController(req, res)
+  },
+)
 
 carRoutes.get('/', (req, res) => {
   readCarsController(req, res)
