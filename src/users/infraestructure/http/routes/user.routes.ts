978c0c9ad109f6998ed2d8bb8abe0945b9e2f850
Arthur-Peixoto <ggpeaol@gmail.com>
2024-11-01 13:39:30 -0300

    feat: add login route

--- src/users/infraestructure/http/routes/user.routes.ts
+++ src/users/infraestructure/http/routes/user.routes.ts
@@ -4,6 +4,7 @@ import { listUsersController } from '../controllers/read-users.controller'
 import { getUserByIdController } from '../controllers/read-single-user.controller'
 import { updateUserController } from '../controllers/update-user.controller'
 import { deleteUserController } from '../controllers/delete-user.controller'
+import { loginUserController } from '../controllers/login.controller'
 const userRoutes = Router()
 
 userRoutes.post('/users/', (req, res) => {
@@ -26,6 +27,8 @@ userRoutes.delete('/users/:id', (req, res) => {
   deleteUserController(req, res);
 });
 
-
+userRoutes.post('/signup', (req, res) => {
+  loginUserController(req, res);
+});
 
 export { userRoutes }
