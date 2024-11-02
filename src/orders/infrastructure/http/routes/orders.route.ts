8af02305b0c5f77376e15d4257fff7556fc983de
Kaio <kaiofernando05cont@gmail.com>
2024-11-02 10:10:26 -0300

    feat: add get order by id route

--- src/orders/infrastructure/http/routes/orders.route.ts
+++ src/orders/infrastructure/http/routes/orders.route.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express'
 import { createOrderController } from '../controllers/create-order.controller'
+import { readOrderController } from '../controllers/read-order.controller'
 
 const orderRoutes = Router()
 
 orderRoutes.post('/', (req, res) => {
     createOrderController(req, res)
+})
+
+orderRoutes.get('/:id', (req, res) => {
+    readOrderController(req, res)
 })
 
 export {orderRoutes}
