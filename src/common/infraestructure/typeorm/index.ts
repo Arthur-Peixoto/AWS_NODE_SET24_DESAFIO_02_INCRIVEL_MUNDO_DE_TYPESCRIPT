280756c08b43564c0e3cd4ad998c4dbaecf720b9
Samuelaraujo10 <samuelaraujo71@gmail.com>
2024-11-02 17:37:11 -0300

    feat: add customer-entity

--- src/common/infraestructure/typeorm/index.ts
+++ src/common/infraestructure/typeorm/index.ts
@@ -4,6 +4,7 @@ import { CreateItems1730228906205 } from './migrations/1730228906205-CreateItems
 import { CreateOrders1730237813958 } from './migrations/1730237813958-CreateOrders'
 import { CreateUsers1730228906206 } from './migrations/1730250492317-CreateUsers'
 import { CreateTokens1730228906207 } from './migrations/1730251259678-CreateTokens'
+import { CreateCustomers1730485886879 } from './migrations/1730485886879-CreateCustomers'
 import { Car } from '@/cars/infraestructure/typeorm/entities/cars.entity'
 import { Item } from '@/cars/infraestructure/typeorm/entities/items.entity'
 
@@ -18,6 +19,7 @@ export const dataSource = new DataSource({
     CreateCars1730228276828,
     CreateItems1730228906205,
     CreateUsers1730228906206,
+    CreateCustomers1730485886879,
     CreateTokens1730228906207,
     CreateOrders1730237813958,
   ],
