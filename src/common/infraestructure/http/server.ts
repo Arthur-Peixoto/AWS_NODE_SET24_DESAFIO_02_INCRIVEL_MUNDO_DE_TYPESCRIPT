04d84f752ce29c6a2a61e2b14a89854b27a7559d
Caio Tavares <102376017+Caio-T4vares@users.noreply.github.com>
2024-11-02 10:19:02 -0300

    refactor: add middleware function direclty to app

--- src/common/infraestructure/http/server.ts
+++ src/common/infraestructure/http/server.ts
@@ -1,8 +1,11 @@
-import express from 'express'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-nocheck
+import express, { NextFunction, Request, Response } from 'express'
 import 'express-async-errors'
 import { dataSource } from '../typeorm'
 import routes from './routes'
-import errorHandler from './middlewares/errorHandler'
+import { AppError } from '@/common/domain/errors/app-error'
+import { errors } from 'celebrate'
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -11,10 +14,25 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use(routes)
+app.use(errors())
 
-app.use((err, req, res, next) => {
-  errorHandler(err, req, res, next)
-})
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      })
+    }
+
+    console.log(error)
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    })
+  },
+)
 
 dataSource.initialize().then(() => {
   app.listen(PORT, () => {
