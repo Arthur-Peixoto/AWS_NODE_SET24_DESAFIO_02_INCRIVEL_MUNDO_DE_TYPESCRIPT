4914a9480e2dc763c6f2858fd562bcb7707bf3a4
Caio Tavares <102376017+Caio-T4vares@users.noreply.github.com>
2024-11-02 09:38:33 -0300

    feat: add middleware to errorHandler

--- src/common/infraestructure/http/server.ts
+++ src/common/infraestructure/http/server.ts
@@ -1,6 +1,8 @@
 import express from 'express'
+import 'express-async-errors'
 import { dataSource } from '../typeorm'
 import routes from './routes'
+import errorHandler from './middlewares/errorHandler'
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -9,6 +11,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use(routes)
+
+app.use((err, req, res, next) => {
+  errorHandler(err, req, res, next)
+})
 
 dataSource.initialize().then(() => {
   app.listen(PORT, () => {
