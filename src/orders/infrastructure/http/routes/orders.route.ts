1177e5edd1ea683a67555b114e6425a92064fa6f
Kaio <kaiofernando05cont@gmail.com>
2024-11-02 15:33:45 -0300

    feat: add delete order route

--- src/orders/infrastructure/http/routes/orders.route.ts
+++ src/orders/infrastructure/http/routes/orders.route.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express'
 import { createOrderController } from '../controllers/create-order.controller'
 import { readOrderController } from '../controllers/read-order.controller'
+// import { readOrdersController } from '../controllers/read-orders.controller'
+import { celebrate, Joi, Segments } from 'celebrate'
+import { deleteOrderController } from '../controllers/delete-order.controller'
 
 const orderRoutes = Router()
 
@@ -8,8 +11,20 @@ orderRoutes.post('/', (req, res) => {
     createOrderController(req, res)
 })
 
+// orderRoutes.get('/', (req, res) => {
+//     readOrdersController(req, res)
+// })
+
 orderRoutes.get('/:id', (req, res) => {
     readOrderController(req, res)
 })
+
+orderRoutes.delete(
+    '/:id',
+    celebrate({ [Segments.PARAMS]: { id: Joi.string().uuid().required() } }),
+    (req, res, next) => {
+      deleteOrderController(req, res, next)
+    },
+  )
 
 export {orderRoutes}
