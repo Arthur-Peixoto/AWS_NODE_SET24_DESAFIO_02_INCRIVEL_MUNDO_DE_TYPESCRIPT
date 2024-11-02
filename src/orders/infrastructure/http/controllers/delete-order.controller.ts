bed314cfe55cc2e0377198c7198792c31d43f72a
Kaio <kaiofernando05cont@gmail.com>
2024-11-02 15:33:16 -0300

    feat: add delete order controller

---
+++ src/orders/infrastructure/http/controllers/delete-order.controller.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from 'express'
+import { dataSource } from '@/common/infraestructure/typeorm'
+import { DeleteOrderUseCase } from '@/orders/application/usecases/delete-order.usecase'
+import { OrdersTypeormRepository } from '../../typeorm/entities/repositories/orders-typeorm.repository'
+import { Order } from '../../typeorm/entities/entities/orders.entity'
+
+export async function deleteOrderController(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const { id } = req.params
+    const deleteOrderUseCase = new DeleteOrderUseCase(
+      new OrdersTypeormRepository(dataSource.getRepository(Order)),
+    )
+
+    const deletedOrder = await deleteOrderUseCase.execute(id)
+
+    return res.status(200).json(deletedOrder)
+  } catch (err) {
+    next(err)
+  }
+}
