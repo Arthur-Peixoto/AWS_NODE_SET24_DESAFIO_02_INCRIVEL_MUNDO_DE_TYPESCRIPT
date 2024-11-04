61e95bab095ea517fa91a36b51063f8209708b87
Arthur-Peixoto <ggpeaol@gmail.com>
2024-11-04 16:17:04 -0300

    fix: add userRoutes in routes

--- src/common/infraestructure/http/routes.ts
+++ src/common/infraestructure/http/routes.ts
@@ -1,10 +1,12 @@
 import { carRoutes } from '@/cars/infraestructure/http/routes/cars.route'
 import customerRouter from '@/modules/customer/infraestructure/http/routes/customer.route'
 import { orderRoutes } from '@/orders/infrastructure/http/routes/orders.route'
+import { userRoutes } from '@/users/infraestructure/http/routes/user.routes'
 import { Router } from 'express'
 
 const routes = Router()
 
+routes.use('/users', userRoutes)
 routes.use('/cars', carRoutes)
 routes.use('/orders', orderRoutes)
 routes.use('/customers', customerRouter)
