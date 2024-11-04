861fb2ffbcbd33c1adf963e2f21ab0fbbb46badd
Caio Tavares <102376017+Caio-T4vares@users.noreply.github.com>
2024-11-04 16:26:19 -0300

    feat: add controller for update costumers

--- src/modules/customer/usecases/update-customer.usecase.ts
+++ src/modules/customer/usecases/update-customer.usecase.ts
@@ -1,55 +1,44 @@
-import { CustomerRepository } from "../domain/repositories/customer.repository";
-import { CustomerModel } from "../domain/models/customer.model";
-import { AppError } from '@/common/domain/errors/app-error';
+import { CustomerRepository } from '../domain/repositories/customer.repository'
+import { CustomerModel } from '../domain/models/customer.model'
+import { AppError } from '@/common/domain/errors/app-error'
 
 export type UpdateCustomerInput = {
-  id: string;
-  fullName?: string;
-  email?: string;
-  cpf?: string;
-};
-
-export type UpdateCustomerOutput = {
-  id: string;
-  fullName: string;
-  email: string;
-  cpf: string;
-};
+  dateBirth?: Date
+  phone?: string
+  fullName?: string
+  email?: string
+  cpf?: string
+}
 
 export class UpdateCustomerUseCase {
   constructor(private customerRepository: CustomerRepository) {}
 
-  async execute(input: UpdateCustomerInput): Promise<UpdateCustomerOutput> {
-
-    const customer = await this.customerRepository.findByID(input.id);
-    if (!customer || customer.deletionDate) {
-      throw new AppError('Cliente inexistente ou já removido', 404);
+  async execute(input: UpdateCustomerInput): Promise<CustomerModel> {
+    const customer = await this.customerRepository.findByID(input.id)
+    if (!customer || customer.deleted_at) {
+      throw new AppError('Customer not found', 404)
     }
 
     if (input.email && input.email !== customer.email) {
-      const duplicatedEmail = await this.customerRepository.findByEmail(input.email);
+      const duplicatedEmail = await this.customerRepository.findByEmail(
+        input.email,
+      )
       if (duplicatedEmail && duplicatedEmail.id !== customer.id) {
-        throw new AppError('Email já cadastrado', 409);
+        throw new AppError('Customer already exist', 409)
       }
     }
 
-    
     const updatedData: CustomerModel = {
       ...customer,
       fullName: input.fullName ?? customer.fullName,
       email: input.email ?? customer.email,
-    };
-
-    const updatedCustomer = await this.customerRepository.update(updatedData);
-    if (!updatedCustomer) {
-      throw new AppError('Erro ao atualizar usuário', 500);
     }
 
-    return {
-      id: updatedCustomer.id,
-      fullName: updatedCustomer.fullName,
-      email: updatedCustomer.email,
-      cpf: updatedCustomer.cpf,
-    };
+    const updatedCustomer = await this.customerRepository.update(updatedData)
+    if (!updatedCustomer) {
+      throw new AppError('Internal server error', 500)
+    }
+
+    return updatedCustomer
   }
 }
