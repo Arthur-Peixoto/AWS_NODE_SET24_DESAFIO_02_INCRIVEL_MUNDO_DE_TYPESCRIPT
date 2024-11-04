558181eb71d65faa52430aa0f80e4442ef021604
Caio Tavares <102376017+Caio-T4vares@users.noreply.github.com>
2024-11-04 15:15:00 -0300

    feat: add customerRouter in general routes

--- src/orders/infrastructure/http/controllers/create-order.controller.ts
+++ src/orders/infrastructure/http/controllers/create-order.controller.ts
@@ -3,31 +3,29 @@ import { OrdersTypeormRepository } from '../../typeorm/repositories/orders-typeo
 import { dataSource } from '@/common/infraestructure/typeorm'
 import { Order } from '../../typeorm/entities/orders.entity'
 import { CreateOrderUseCase } from '@/orders/application/usecases/create-order.usecase'
-import { CustomersTypeormRepository } from '@/modules/customer/typeorm/repositories/customers.typeorm.repository'
-import Customer from '@/modules/customer/typeorm/entities/customer.entity'
-
+import Customer from '@/modules/customer/infraestructure/typeorm/entities/customer.entity'
+import { CustomersTypeormRepository } from '@/modules/customer/infraestructure/typeorm/repositories/customers.typeorm.repository'
 
 export async function createOrderController(
   request: Request,
   response: Response,
-  next: NextFunction
+  next: NextFunction,
 ) {
   try {
     const { car, customer } = request.body
 
-  const createOrderUseCase = new CreateOrderUseCase.UseCase(
-    new OrdersTypeormRepository(dataSource.getRepository(Order)),
-    new CustomersTypeormRepository(dataSource.getRepository(Customer))
-  )
+    const createOrderUseCase = new CreateOrderUseCase.UseCase(
+      new OrdersTypeormRepository(dataSource.getRepository(Order)),
+      new CustomersTypeormRepository(dataSource.getRepository(Customer)),
+    )
 
-  const order = await createOrderUseCase.execute({
-    car,
-    customer,
-  })
+    const order = await createOrderUseCase.execute({
+      car,
+      customer,
+    })
 
-  return response.status(201).json(order)
+    return response.status(201).json(order)
   } catch (err) {
     next(err)
   }
-  
 }
