cc6ea7a79f42335552aa208aa865df2c7dc40e55
Arthur-Peixoto <ggpeaol@gmail.com>
2024-11-04 13:48:59 -0300

    ref: add error handler in read single user usecase

--- src/users/usecases/read-single-user.usecase.ts
+++ src/users/usecases/read-single-user.usecase.ts
@@ -1,12 +1,15 @@
 import { UserRepository } from '@/users/domain/repositories/users.repository';
 import { UserModel } from '@/users/domain/models/users.model';
+import { AppError } from '@/common/domain/errors/app-error';
 
 export class GetUserByIdUseCase {
   constructor(private userRepository: UserRepository) {}
 
   async execute(id: string): Promise<UserModel> {
     const user = await this.userRepository.findByID(id);
-    if (!user) throw new Error('User not found');
+    if (!user) {
+      throw new AppError('Usuário não encontrado', 404);
+    }
     return user;
   }
 }
